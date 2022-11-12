27df5cf1691abac76b5611a34674769b9f4f8313
Ananiya <ananiya123416@gmail.com>
2022-11-12 09:17:36 +0300

    owlcrousel is functional
    image animation and position need
    to be precise

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js" integrity="sha512-gY25nC63ddE0LcLPhxUJGFxa2GoIyA5FLym4UJqHDEMHjp8RET6Zn/SHo1sltt3WuVtqfyxECP38/daUc/WVEA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+//<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="./owl.carousel.min.js"></script>
+
 var menulist = document.getElementById('menulist');
 menulist.style.maxHeight=="0px";
 
@@ -10,4 +13,8 @@ function menutoggle(){
         menulist.style.maxHeight=="0px";
     }
 }
-
+/*
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+    });
+  */  
